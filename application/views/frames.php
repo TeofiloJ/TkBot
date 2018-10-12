7557319d688c85f79fefc2f4d57bf64ee551e7dc
Téofilo <teofilo.jeandot@gmail.com>
2018-10-12 14:58:10 +0200

    ajout des images dynamiques

--- application/views/frames.php
+++ application/views/frames.php
@@ -28,13 +28,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
           </div>
         </form>
 
-      <div class='row border'>
-        <div class='col-sm-6'>
+      
+          <script>
+   function getCharacter(character){
+      character = character?character+"/":"";
+      var test = escape("<?php echo base_url()?>" + "index.php/api/characters/" + character);
+      console.log(test);
+      jQuery.ajax({
+      type: "GET",
+      url: "<?php echo base_url()?>" + "index.php/api/characters/" + character,
+      dataType: 'json',
+      success: function(data) {
+      var char = document.getElementById("character")
 
-        </div>
-        <div class='col-sm-6'>
-          <img class='img-fluid portrait' src='https://i.imgur.com/aOHX45D.jpg'/>
-        </div>
+      var nom = document.getElementById("nom");
+      nom.innerText=data[0].proper_name;
+      var image = document.getElementById("image");
+      image.src=data[0].portrait;
+
+        console.log(data);      
+
+      }
+    })
+    }
+    </script>
+    <div class='row' id='character'>
+          <img id="image" class='img-fluid portrait mb-2' src=''/>
+          <p id="nom" class='col mb-2'></p>       
       </div>
 
     <div class="results">
@@ -63,26 +83,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         $("#character").change(function(event) {
           event.preventDefault();
           var character = $("input#character").val();
-          var command = $("input#command").val();     
+          var command = $("input#command").val();
+          getCharacter(character);     
           getData(character, command);
+          
         
         });
       
         $("#command").change(function(event) {
           event.preventDefault();
           var character = $("input#character").val();
-          var command = $("input#command").val();     
+          var command = $("input#command").val();
+          getCharacter(character);     
           getData(character, command);
         
         });
 
-
     function getData(character, command){
       character = character?character+"/":"";
-      console.log("C " + character );
-      console.log("com" + command);
       command = command?command:"";
-      var test = "<?php echo base_url()?>" + "index.php/api/movesets/" + character + command;
+      var test = "<?php echo base_url()?>" + "index.php/api/movesets/" +escape( character + command);
       console.log(test);
       jQuery.ajax({
       type: "GET",
@@ -96,11 +116,33 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         console.log(data);
         for (var ligne of data){
           var row = document.createElement("tr");
-          for (var element in ligne){
-            var cell = document.createElement("td");
-                cell.innerText = ligne[element];
-                row.appendChild(cell);
-          }
+
+          var cell = document.createElement("td");
+          cell.innerText = ligne.command;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.hit_level;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.damage;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.start_up_frame;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.block_frame;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.hit_frame;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.counter_hit_frame;
+          row.appendChild(cell);
+          var cell = document.createElement("td");
+          cell.innerText = ligne.notes;
+          row.appendChild(cell);
+
+
           tab.appendChild(row);
         }
 
